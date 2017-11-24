29a31f4c7f86105c51243f8200282c92f4d4ef0f
albert85 <tmtpalbert@gmail.com>
2017-11-24 05:04:36 +0100

    new test folder

---
+++ database.js
@@ -0,0 +1,42 @@
+const eventDatabase = {
+  user: [
+    {
+      id: 0,
+      name: 'wedding',
+      location: 'ikeja',
+      startTime: '1.00AM',
+      endTime: '2.00AM',
+    },
+    {
+      id: 1,
+      name: 'burial',
+      location: 'Lagos Island',
+      startTime: '1.00AM',
+      endTime: '2.00AM',
+    },
+    {
+      id: 2,
+      name: 'conference',
+      location: 'Ikorodu',
+      startTime: '1.00AM',
+      endTime: '2.00AM',
+    },
+    {
+      id: 3,
+      name: 'meeting',
+      location: 'Victoria Island',
+      startTime: '1.00AM',
+      endTime: '2.00AM',
+    },
+  ],
+  admin: [{
+    id: 0,
+    name: 'meeting',
+    location: 'Victoria Island',
+    capacity: 200,
+    amount: 100,
+  },
+  ],
+};
+
+export default eventDatabase;
---
+++ src/app.js
@@ -0,0 +1,5 @@
+const test = 4;
+
+for(let x of [1,2,3]){
+    console.log(x);
+}
